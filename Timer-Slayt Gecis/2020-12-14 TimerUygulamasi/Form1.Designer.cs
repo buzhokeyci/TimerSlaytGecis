216270ef3d79f3c0d9e0c7e5af08dae60b6d8eb0
Enes Mert Kaya <127692367+buzhokeyci@users.noreply.github.com>
2023-07-10 10:44:50 +0300

    TimerSlaytGecis
    Zamanlayici ile gecen slaylar.

---
+++ Timer-Slayt Gecis/2020-12-14 TimerUygulamasi/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿
+namespace _2020_12_14_TimerUygulamasi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnFirst = new System.Windows.Forms.Button();
+            this.btnPrevious = new System.Windows.Forms.Button();
+            this.btnStartStop = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnLast = new System.Windows.Forms.Button();
+            this.pcbPicture = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.imgList = new System.Windows.Forms.ImageList(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pcbPicture)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnFirst
+            // 
+            this.btnFirst.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnFirst.Location = new System.Drawing.Point(0, 19);
+            this.btnFirst.Name = "btnFirst";
+            this.btnFirst.Size = new System.Drawing.Size(112, 101);
+            this.btnFirst.TabIndex = 0;
+            this.btnFirst.Text = "|<";
+            this.btnFirst.UseVisualStyleBackColor = true;
+            this.btnFirst.Click += new System.EventHandler(this.btnFirst_Click);
+            // 
+            // btnPrevious
+            // 
+            this.btnPrevious.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnPrevious.Location = new System.Drawing.Point(118, 19);
+            this.btnPrevious.Name = "btnPrevious";
+            this.btnPrevious.Size = new System.Drawing.Size(123, 101);
+            this.btnPrevious.TabIndex = 0;
+            this.btnPrevious.Text = "<<";
+            this.btnPrevious.UseVisualStyleBackColor = true;
+            this.btnPrevious.Click += new System.EventHandler(this.btnPrevious_Click);
+            // 
+            // btnStartStop
+            // 
+            this.btnStartStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnStartStop.Location = new System.Drawing.Point(247, 17);
+            this.btnStartStop.Name = "btnStartStop";
+            this.btnStartStop.Size = new System.Drawing.Size(592, 100);
+            this.btnStartStop.TabIndex = 0;
+            this.btnStartStop.Text = "START - STOP";
+            this.btnStartStop.UseVisualStyleBackColor = true;
+            this.btnStartStop.Click += new System.EventHandler(this.btnStartStop_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnNext.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnNext.Location = new System.Drawing.Point(845, 18);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(129, 99);
+            this.btnNext.TabIndex = 0;
+            this.btnNext.Text = ">>";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnLast
+            // 
+            this.btnLast.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnLast.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnLast.Location = new System.Drawing.Point(974, 18);
+            this.btnLast.Name = "btnLast";
+            this.btnLast.Size = new System.Drawing.Size(129, 99);
+            this.btnLast.TabIndex = 0;
+            this.btnLast.Text = ">|";
+            this.btnLast.UseVisualStyleBackColor = true;
+            this.btnLast.Click += new System.EventHandler(this.btnLast_Click);
+            // 
+            // pcbPicture
+            // 
+            this.pcbPicture.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pcbPicture.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pcbPicture.Location = new System.Drawing.Point(0, 0);
+            this.pcbPicture.Name = "pcbPicture";
+            this.pcbPicture.Size = new System.Drawing.Size(1106, 418);
+            this.pcbPicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pcbPicture.TabIndex = 1;
+            this.pcbPicture.TabStop = false;
+            this.pcbPicture.Click += new System.EventHandler(this.pcbPicture_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnFirst);
+            this.groupBox1.Controls.Add(this.btnPrevious);
+            this.groupBox1.Controls.Add(this.btnStartStop);
+            this.groupBox1.Controls.Add(this.btnNext);
+            this.groupBox1.Controls.Add(this.btnLast);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(0, 418);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1106, 120);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // imgList
+            // 
+            this.imgList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imgList.ImageStream")));
+            this.imgList.TransparentColor = System.Drawing.Color.Transparent;
+            this.imgList.Images.SetKeyName(0, "1.jpg");
+            this.imgList.Images.SetKeyName(1, "2.jpg");
+            this.imgList.Images.SetKeyName(2, "3.jpg");
+            this.imgList.Images.SetKeyName(3, "4.jpg");
+            this.imgList.Images.SetKeyName(4, "1_XBC9diIIlWIbbE8BVXgulA.jpeg");
+            this.imgList.Images.SetKeyName(5, "5.jpeg");
+            this.imgList.Images.SetKeyName(6, "6.jpg");
+            this.imgList.Images.SetKeyName(7, "ca_pugcoronavirus_042820istock.jpg");
+            this.imgList.Images.SetKeyName(8, "pug.png");
+            this.imgList.Images.SetKeyName(9, "pug-kopek-ozellikleri.jpg");
+            this.imgList.Images.SetKeyName(10, "pug-manşet-600x300.jpg");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1106, 538);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pcbPicture);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pcbPicture)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFirst;
+        private System.Windows.Forms.Button btnPrevious;
+        private System.Windows.Forms.Button btnStartStop;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnLast;
+        private System.Windows.Forms.PictureBox pcbPicture;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ImageList imgList;
+    }
+}
+
---
+++ Timer-Slayt Gecis/2020-12-14 TimerUygulamasi/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2020_12_14_TimerUygulamasi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random rnd = new Random();
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //Timer sadece ekleme ve olayını oluşturma ile çalışmaya başlamaz. Bu işlemleri yaptıktan sonra başla demek zorundasınız.
+            //Tick anında yazılan kodları, belirtilen sürede bir çalıştırır.
+            //MessageBox.Show("Test");
+
+            int index = rnd.Next(0, imgList.Images.Count);
+
+            pcbPicture.Image = imgList.Images[index];
+
+            pcbPicture.Tag = index;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+            this.Text = "Timer = Aktif";
+        }
+
+        private void btnStartStop_Click(object sender, EventArgs e)
+        {
+            //timer1.Enabled
+            if (timer1.Enabled)
+            {
+                timer1.Stop();
+                this.Text = "Timer = Pasif";
+            }
+            else
+            {
+                timer1.Start();
+                this.Text = "Timer = Aktif";
+            }
+        }
+
+        private void btnFirst_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled)
+                timer1.Stop();
+            pcbPicture.Image = imgList.Images[0];
+            pcbPicture.Tag = 0;
+        }
+
+        private void btnLast_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled)
+                timer1.Stop();
+            pcbPicture.Image = imgList.Images[imgList.Images.Count - 1];
+            pcbPicture.Tag = imgList.Images.Count - 1;
+        }
+
+        private void btnPrevious_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled) // eğer timer çalışıyosa bu resmi değiştirecektir. biz değiştirmesin son atacağımız resim ekranda kalsın istiyoruz. bu nedenle timer çalışıyorsa durduralım
+                timer1.Stop();
+
+            int sonResminIndexi = Convert.ToInt32(pcbPicture.Tag);
+            sonResminIndexi--;
+
+            if (Math.Sign(sonResminIndexi) == -1)
+                sonResminIndexi = imgList.Images.Count - 1;
+
+            pcbPicture.Image = imgList.Images[sonResminIndexi];
+
+            pcbPicture.Tag = sonResminIndexi;
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            int index = Convert.ToInt32(pcbPicture.Tag);
+            index++;
+
+            if (index == imgList.Images.Count)
+                index = 0;
+
+            pcbPicture.Image = imgList.Images[index];
+
+            pcbPicture.Tag = index;
+        }
+
+        private void pcbPicture_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
